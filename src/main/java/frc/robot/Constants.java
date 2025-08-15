d1fe4180a94b3f07d74c1b736e0e4e4f69111f3e
ariakrishnamoorthy <aria.r.krishnamoorthy@gmail.com>
2025-08-14 20:18:39 -0700

    change motor voltage

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,6 +1,6 @@
 package frc.robot;
 public class Constants {
     public static final int MOTOR_ID = 1;
-    public static final double MOTOR_VOLTAGE = 0.0;
+    public static final double MOTOR_VOLTAGE = 1.0;
     public static final int CONTROLLER_PORT = 0;
 }
