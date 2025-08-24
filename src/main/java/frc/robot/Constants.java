7a244369fdf7021e9cff2ad83d028fcd52b52938
anisha-bajaj <anisha.bajaj@icloud.com>
2025-08-24 15:34:12 -0700

    add code to run motor 2

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+public class Constants {
+    public static final int MOTOR_ID = 2;
+
+    public static final double VOLTAGE = 1;
+
+    public static final int JOYSTICK_PORT = 0;
+}
+
+
+    
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,7 +1,14 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.ExampleSubsystem;
+
 public class RobotContainer {
+    private final ExampleSubsystem robotMotor = new ExampleSubsystem();
+    private final CommandXboxController joystick = new CommandXboxController(Constants.JOYSTICK_PORT);
     public RobotContainer() {
+        joystick.a().whileTrue(robotMotor.runMotorCommand());
+        robotMotor.setDefaultCommand(robotMotor.stopMotorCommand());
 
     }
 }
